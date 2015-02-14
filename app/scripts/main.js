b9dcea43f88eaafc05ecfd40d2026fd0931c3bdb
skorlir <skorlir@gmail.com>
2015-02-14 17:06:41 -0600

    add background header video placeholder

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,14 @@
 /* jshint devel:true */
 
 var loaderImg = $('img#main-logo');
+var video     = $('section#main video');
 
 $('#page-loader').click(function() {
   var overlay = $(this);
   overlay.addClass('animate');
   loaderImg.addClass('animate');
+
+  video[0].play();
 
   setTimeout(function() {
     overlay.addClass('loaded');
